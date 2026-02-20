6e0411f006048d00af0edc90f3011394201f8f1a
Galin <galin.iliev96@gmail.com>
2026-02-20 13:33:20 +0200

    Fix PORT for Render

--- server/server.js
+++ server/server.js
@@ -77,7 +77,8 @@ app.post("/data", (req, res) => {
   }
 });
 
-app.listen(5000, "0.0.0.0", () => {
-  console.log("Server running on http://localhost:5000");
-});
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
